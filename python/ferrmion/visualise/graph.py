e94e333e528058184ae91abb32b75e5d5f86aa91
MIWdlB <m.i.w@outlook.com>
2025-06-12 22:02:09 +0100

    format graph vis

--- python/ferrmion/visualise/graph.py
+++ python/ferrmion/visualise/graph.py
@@ -1,18 +1,17 @@
 """Graph visualisation tools."""
 
-from typing import Mapping
-
 import rustworkx as rx
 from rustworkx.visualization import mpl_draw
 
 from ferrmion.encode.ternary_tree_node import node_sorter
 
 
-def draw_tt(graph: rx.PyDiGraph) -> None:
+def draw_tt(graph: rx.PyDiGraph, enumeration_scheme=None):
     """Draws a rustworkx graph with nodes positioned as a ternary tree.
 
     Args:
         graph (rustworkx.PyDiGraph): A ternary tree graph.
+        enumeration_scheme (dict[str, tuple[int, int]]): A mapping from node labels to a tuple of (mode index, qubit index).
 
     Example:
         tree = ferrmion.encode.TernaryTree(10).BK()
@@ -30,17 +29,21 @@ def draw_tt(graph: rx.PyDiGraph) -> None:
             ]
         )
 
-    posmap: Mapping[int, tuple[float, float]] = {
-        index: (x_pos(label), y_pos(label)) for index, label in enumerate(graph.nodes())
+    def format_label(label):
+        return rf"$f_{{{enumeration_scheme[label][0]}}}q_{{{enumeration_scheme[label][1]}}}$"
+
+    posmap = {
+        index: [x_pos(label), y_pos(label)] for index, label in enumerate(graph.nodes())
     }
-    # set the root node to the origin
-    posmap[0] = (0, 0)
+    posmap[0] = [0, 0]
+    labels: callable = str if enumeration_scheme is None else format_label
 
     mpl_draw(
         graph,
         pos=posmap,
         with_labels=True,
         node_size=600,
-        node_color="teal",
+        node_color="orange",
         edge_labels=str,
+        labels=labels,
     )

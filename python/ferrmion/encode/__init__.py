fba27a5457347475eb17f7ebc56f8a5678688406
MIWdlB <m.i.w@outlook.com>
2025-09-04 10:50:09 +0100

    update tt notebook and module imports

--- python/ferrmion/encode/__init__.py
+++ python/ferrmion/encode/__init__.py
@@ -2,10 +2,24 @@
 
 from .base import FermionQubitEncoding
 from .knto import KNTO
-from .ternary_tree import TernaryTree
+from .ternary_tree import (
+    BK,
+    JKMN,
+    JW,
+    BravyiKitaev,
+    JordanWigner,
+    ParityEncoding,
+    TernaryTree,
+)
 
 __all__ = [
     "FermionQubitEncoding",
     "TernaryTree",
     "KNTO",
+    "JordanWigner",
+    "JW",
+    "BravyiKitaev",
+    "BK",
+    "ParityEncoding",
+    "JKMN",
 ]

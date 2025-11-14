3b527fc0c0042d73173c3bdcd3c167e5cf9c0269
MIWdlB <m.i.w@outlook.com>
2025-11-14 09:17:52 +0000

    remove lifetimes frm struct defs

--- src/types.rs
+++ src/types.rs
@@ -1,9 +1,11 @@
-use std::ops::Mul;
+use ndarray::Axis;
 /*
 Shared Types.
 */
-use numpy::ndarray::{Array2, ArrayView1, ArrayView2};
+use crate::utils::vector_kron;
+use numpy::ndarray::{arr1, arr2, Array1, Array2, ArrayView1};
 use numpy::Complex64;
+use std::collections::HashMap;
 use std::{result::Result, str::FromStr};
 
 #[allow(dead_code)]
@@ -39,6 +41,17 @@ impl FromStr for LadderOperator {
     }
 }
 
+impl LadderOperator {
+    pub fn fermion_coeff(&self) -> Array1<Complex64> {
+        match &self {
+            LadderOperator::Creation => arr1(&[Complex64::new(0.5, 0.0), Complex64::new(0., -0.5)]),
+            LadderOperator::Annihilation => {
+                arr1(&[Complex64::new(0.5, 0.0), Complex64::new(0., 0.5)])
+            }
+        }
+    }
+}
+
 #[cfg(test)]
 mod ladder_tests {
     use crate::types::*;
@@ -61,10 +74,12 @@ mod ladder_tests {
 Fermion
 */
 
-#[derive(Debug, PartialEq)]
-struct FermionHamiltonian<'coeff> {
-    terms: Vec<(Vec<LadderOperator>, ArrayView2<'coeff, f64>)>,
-}
+// #[derive(Debug, PartialEq)]
+// struct FermionHamiltonian<'coeff> {
+//     terms: Vec<(Vec<LadderOperator>, Array
+//
+// View2<'coeff, f64>)>,
+// }
 
 #[derive(Debug, PartialEq, Clone, Copy)]
 struct FermionOperator {
@@ -78,71 +93,43 @@ impl FermionOperator {
     }
 }
 
-impl Mul for FermionOperator {
-    type Output = FermionProduct;
-
-    fn mul(self, rhs: Self) -> Self::Output {
-        let mut ops = Vec::<FermionOperator>::with_capacity(4);
-        let mut coeff = Complex64::new(1., 0.);
-        if self.index >= rhs.index {
-            ops.push(self);
-            ops.push(rhs);
-        } else {
-            ops.push(rhs);
-            ops.push(self);
-            coeff *= -1.;
-        };
-        FermionProduct::new(ops, Some(coeff))
-    }
+struct SparseFermionTerm {
+    signature: Vec<LadderOperator>,
+    indices: Array2<u16>,
+    coefficients: Array1<Complex64>,
 }
 
-impl Mul<FermionProduct> for FermionOperator {
-    type Output = FermionProduct;
-    fn mul(self, rhs: FermionProduct) -> Self::Output {
-        rhs.ops.push(self);
-    }
-}
+#[derive(Debug, PartialEq, Clone)]
+struct SparseFermionError;
 
-#[derive(Debug, PartialEq)]
-struct FermionProduct {
-    ops: Vec<FermionOperator>,
-    coeff: Complex64,
-}
-
-impl FermionProduct {
-    pub fn new(ops: Vec<FermionOperator>, coeff: Option<Complex64>) -> Self {
-        match coeff {
-            Some(val) => Self {
-                ops: ops,
-                coeff: val,
-            },
-            None => Self {
-                ops: ops,
-                coeff: Complex64::new(1., 0.),
-            },
-        }
-    }
-}
-
-struct SparseFermionHamiltonian<'coeff> {
-    terms: Array2<FermionOperator>,
-    coefficients: ArrayView1<'coeff, Complex64>,
-}
-
-impl<'coeff> SparseFermionHamiltonian<'coeff> {
+impl SparseFermionTerm {
     pub fn new(
-        terms: Array2<FermionOperator>,
-        coefficients: ArrayView1<'coeff, Complex64>,
-    ) -> Self {
-        Self {
-            terms,
+        signature: Vec<LadderOperator>,
+        indices: Array2<u16>,
+        coefficients: Array1<Complex64>,
+    ) -> Result<Self, SparseFermionError> {
+        if coefficients.len() != indices.len_of(Axis(1))
+            || signature.len() != indices.len_of(Axis(0))
+        {
+            return Err(SparseFermionError);
+        };
+
+        Ok(Self {
+            signature,
+            indices,
             coefficients,
-        }
+        })
     }
+}
+
+struct SparseFermionHamiltonian {
+    terms: Vec<SparseFermionTerm>,
 }
 
 #[cfg(test)]
 mod fermion_tests {
+    use ndarray::arr1;
+
     use crate::types::*;
 
     fn test_fermion_operators() {
@@ -162,34 +149,70 @@ mod fermion_tests {
                 index: 1
             }
         );
-        assert_eq!(c0 * a1, FermionProduct::new(vec![c0, a1], false));
-        assert_eq!(a1 * c0, FermionProduct::new(vec![c0, a1], true));
+    }
+
+    fn test_sparse_term() {
+        let signature = vec![LadderOperator::Creation, LadderOperator::Annihilation];
+        let indices = arr2(&[[0, 1], [2, 3]]);
+        let coefficients = arr1(&[Complex64::new(1.0, 0.), Complex64::new(-1., 0.)]);
+        let term = SparseFermionTerm::new(signature, indices, coefficients).unwrap();
     }
 }
 
 // /*
 // Majorana
 // */
-// struct MajoranaOperator(u32);
+struct SparseMajoranaTerm {
+    indices: Array2<u16>,
+    coefficients: Array1<Complex64>,
+}
 
-// struct MajoranaHamiltonian<'coeff> {
-//     terms: Vec<ArrayView2<'coeff, f64>>,
-// }
+#[derive(Debug, PartialEq, Clone)]
+struct SparseMajoranaError;
 
-// struct SparseMajoranaHamiltonian {
-//     terms: Array2<MajoranaOperator>,
-//     coefficients: Array1<Complex64>,
-// }
+impl SparseMajoranaTerm {
+    pub fn new(
+        indices: Array2<u16>,
+        coefficients: Array1<Complex64>,
+    ) -> Result<Self, SparseMajoranaError> {
+        if coefficients.len() != indices.len_of(Axis(1)) {
+            return Err(SparseMajoranaError);
+        };
+        Ok(Self {
+            indices,
+            coefficients,
+        })
+    }
+}
 
-// impl SparseMajoranaHamiltonian {
-//     pub fn new(terms: Array2<MajoranaOperator>, coefficients: Array1<Complex64>) -> Self {
-//         Self {
-//             terms,
-//             coefficients,
-//         }
-//     }
-// }
+impl From<SparseFermionTerm> for SparseMajoranaTerm {
+    fn from(sft: SparseFermionTerm) -> Self {
+        // Start off by creating a hashmap as we'll need to add a few fermionic terms
+        // to each majorana term
+        let signature_coeffs = sft
+            .signature
+            .iter()
+            .map(|s| s.fermion_coeff())
+            .reduce(|acc, s| vector_kron(&acc, &s));
+        let mut majoranas: HashMap<ArrayView1<u16>, Complex64> = HashMap::new();
+        for fermion_index in sft.indices.rows() {
+            majoranas.entry(fermion_index);
+        }
+        SparseMajoranaTerm::new(sft.indices, sft.coefficients)
+            .expect("Indices and coefficients should be same length.")
+    }
+}
 
-// impl TryFrom<SparseFermionHamiltonian> for SparseMajoranaHamiltonian {
-//     fn try_from(fermion_hamiltonian: SparseFermionHamiltonian) -> Self {}
-// }
+struct SparseMajoranaHamiltonian {
+    terms: Vec<SparseMajoranaTerm>,
+}
+
+#[cfg(test)]
+mod majorana_tests {
+    use crate::types::*;
+    fn test_sparse_term() {
+        let indices = arr2(&[[0, 1], [2, 3]]);
+        let coefficients = arr1(&[Complex64::new(1.0, 0.), Complex64::new(-1., 0.)]);
+        let term = SparseMajoranaTerm::new(indices, coefficients).unwrap();
+    }
+}

6a929aff5b64f6f124889cbef06a38a23ddf858c
Michael Williams de la Bastida <55510238+MIWdlB@users.noreply.github.com>
2025-12-23 08:05:55 +0000

    0.5.1 (#108)

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,7 +18,7 @@
 project = 'ferrmion'
 copyright = '2025, Michael Williams de la Bastida'
 author = 'Michael Williams de la Bastida'
-version = "0.5.0"
+version = "0.5.1"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
